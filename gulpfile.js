15edcd8a19ab7d72fcfcca73b6622e48a88e00d0
David Marek <david@marek.me>
2018-02-13 00:22:11 +0100

    Generate sourcemaps and debug from vscode

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var sourcemaps = require('gulp-sourcemaps');
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+gulp.task('compileTypescript', function() {
+  return tsProject.src()
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .js
+    .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: './' }))
+    .pipe(gulp.dest("dist"));
+})
+
+gulp.task('watch', ['compileTypescript'], function() {
+  gulp.watch(tsProject.config.include, ['compileTypescript']);
 });
+
+gulp.task("default", ['compileTypescript']);
