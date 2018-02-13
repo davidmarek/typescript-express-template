b8f320875315e6e945a2e99dcef9499eed3a2bb4
David Marek <david@marek.me>
2018-02-14 00:03:00 +0100

    Add pug and errorhandler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
+import * as path from "path";
+import * as errorhandler from "errorhandler";
 
 // Load environment variables from .env file.
 dotenv.config();
@@ -7,3 +9,12 @@ dotenv.config();
 export const app = express();
 
 app.set("port", process.env.PORT || 3000);
+
+// Set pug as view engine.
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+
+// Set error handling for development env.
+if (app.get("env") === "development") {
+  app.use(errorhandler());
+}
